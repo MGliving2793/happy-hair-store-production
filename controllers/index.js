44e9e658ae0535458234662f0f49a0da6ed34d77
MGliving2793 <mgliving2793@gmail.com>
2026-08-11 21:24:12 +0530

    Add files via upload

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  authController: require('./auth.controller'),
+  productController: require('./product.controller'),
+  orderController: require('./order.controller'),
+  reviewController: require('./review.controller')
+};
