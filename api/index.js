3e47e5605d6a2f9e4bfb08595dd8302b3c976be3
MGliving2793 <mgliving2793@gmail.com>
2026-08-11 21:42:15 +0530

    Delete api/index.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.use('/auth', require('./auth.routes'));
-router.use('/products', require('./product.routes'));
-router.use('/orders', require('./order.routes'));
-router.use('/payment', require('./payment.routes'));
-router.use('/reviews', require('./review.routes'));
-module.exports = router;
